062077961279f5469cfeb215f53f5dc4afc66b5f
William Xia <59506669+Meteors27@users.noreply.github.com>
2022-09-01 20:24:42 +0800

    first commit

---
+++ GreyScaleSensor.h
@@ -0,0 +1,39 @@
+#ifndef GREYSCALESENSOR_H
+#define GREYSCALESENSOR_H
+#if defined(ARDUINO) && ARDUINO >= 100
+    #include "Arduino.h"
+#else
+    #include "WProgram.h"
+#endif
+
+#define STRAIGHT    1
+#define LEFT        2
+#define EXTRALEFT   3
+#define RIGHT       4
+#define EXTRARIGHT  5
+#define CROSS       6
+
+#define BLACKLINE   0
+#define WHITELINE   1
+
+class GREYSCALESENSOR {
+    public:
+        GREYSCALESENSOR(int left, int middle, int right, int compare, int mode);
+        int detect();
+        int read_sensorL();
+        int read_sensorM();
+        int read_sensorR();
+        int judgeR();
+        int judgeL();
+        int judgeM();
+    private:
+        int LPIN;
+        int MPIN;
+        int RPIN;
+        int LD;
+        int MD;
+        int RD;
+        int CMP;
+        int MODE;
+};
+#endif
