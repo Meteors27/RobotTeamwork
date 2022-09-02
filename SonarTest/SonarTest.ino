b1c810cb5c3d102f95250bdef0b57889e58b49c9
Meteors27 <williamxia27@qq.com>
2022-09-02 23:45:41 +0800

    first commit

---
+++ SonarTest/SonarTest.ino
@@ -0,0 +1,22 @@
+// 1 滤波
+// 2 最远距离 50 cm
+// 3 由于大于最远距离时echopin得到输入 0 cm, 因此有效数值应大于 5 cm
+
+#include <NewPing.h>
+
+#define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 80 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+void setup() {
+  Serial.begin(9600); // Open serial monitor at 115200 baud to see ping results.
+}
+
+void loop() {
+  delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  Serial.print("Ping: ");
+  Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  Serial.println("cm");
+}
